57edfeabe002d59a30d80368501af93badd9e812
airtoncruz <airton.cruz@ciss.com.br>
2020-10-05 14:49:10 -0300

    Criando teste do endpoint de pessoa

---
+++ src/test/java/br/com/softplan/avaliacaoairton/PessoaEndpointTests.java
@@ -0,0 +1,8 @@
+package br.com.softplan.avaliacaoairton;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PessoaEndpointTests {
+
+}
